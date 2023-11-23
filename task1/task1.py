0b22b82af9e40ae207401e63f2821c66be45ae1f
Hobin Kim <hobinkim@Hobinui-MacBookPro.local>
2023-11-23 14:53:53 +0900

    handout revised

--- task1/task1.py
+++ task1/task1.py
@@ -1,11 +1,11 @@
 from aes_utils import *
 
-key = None # generate your own 16 bytes key
-iv = None # generate your own 16 bytes initial vector
+key = bytearray.fromhex('2b7e151628aed2a6abf7158809cf4f3c') # generate your own 16 bytes key
+iv = bytearray.fromhex('000102030405060708090a0b0c0d0e0f') # generate your own 16 bytes initial vector
 
 # fill these two functions and submit this file
-def dcbc_encryption(plain: bytes, key: bytes, iv: bytes) -> bytes:
+def ccbc_encryption(plain: bytes, key: bytes, iv: bytes) -> bytes:
     return None
 
-def dcbc_decryption(cipher: bytes, key: bytes, iv: bytes) -> bytes:
+def ccbc_decryption(cipher: bytes, key: bytes, iv: bytes) -> bytes:
     return None
