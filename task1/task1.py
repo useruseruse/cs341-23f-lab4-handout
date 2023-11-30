418758a07703663ebb91f7a9013b08c0fec98705
useruseruse <eliza1102@kaist.ac.kr>
2023-12-01 01:24:43 +0900

    init

--- task1/task1.py
+++ task1/task1.py
@@ -1,15 +1,51 @@
 from aes_utils import *
+from Crypto.Random import get_random_bytes
 
-key = bytearray.fromhex('2b7e151628aed2a6abf7158809cf4f3c') # generate your own 16 bytes key
-iv = bytearray.fromhex('000102030405060708090a0b0c0d0e0f') # generate your own 16 bytes initial vector
+# key = bytearray.fromhex('2b7e151628aed2a6abf7158809cf4f3c') # generate your own 16 bytes key
+# iv = bytearray.fromhex('000102030405060708090a0b0c0d0e0f') # generate your own 16 bytes initial vector
+
+def generate_random_key_iv():
+    key = bytearray(get_random_bytes(16))  # 16 bytes = 128 bits
+    iv = bytearray(get_random_bytes(16))   # 16 bytes = 128 bits
+    return key, iv
+
+key, iv = generate_random_key_iv()
 
 # fill these two functions
 def ccbc_encryption(plain: bytes, key: bytes, iv: bytes) -> bytes:
-    return None
+    input_list = [plain[i:i+AES_BLOCK_SIZE] for i in range(0, len(plain), AES_BLOCK_SIZE)]
+    encrypted_blocks = []
+
+    carry_in = None
+    while(len(input_list)!=0):
+        if(carry_in == None):
+            carry_in = iv
+        plain_input = input_list.pop(0)
+        block_input = xor_bytes(plain_input, carry_in)
+        block_output = aes_encryption(block_input, key)
+        ciper = xor_bytes(block_output, carry_in)
+        carry_in = xor_bytes(plain_input, ciper)
+        # bytes concatenate
+        encrypted_blocks.append(ciper)
+        
+    return  b''.join(encrypted_blocks)
 
 def ccbc_decryption(cipher: bytes, key: bytes, iv: bytes) -> bytes:
-    return None
+    input_list = [cipher[i:i+AES_BLOCK_SIZE] for i in range(0, len(cipher), AES_BLOCK_SIZE)]
+    carry_in = None
+    decrypted_blocks = []
 
+    while(len(input_list)!=0):
+        if(carry_in==None):
+            carry_in = iv
+        cipher_input = input_list.pop(0)
+        block_input = xor_bytes(cipher_input, carry_in)
+        block_output = aes_decryption(block_input, key)
+        plain = xor_bytes(block_output, carry_in)
+        carry_in = xor_bytes(cipher_input, plain)
+        decrypted_blocks.append(plain)
+
+    return b''.join(decrypted_blocks)
 
 plaintext = bytes('Thank you so much for your hard work during the 23 Fall semester', 'utf-8')
 expected_ciphertext = bytes.fromhex('cf62ae1132f76bb9837e478296ce4e4c0bb2712c2bcc088c24611e264f6178c416f4d8a27f909c0056532ef0193d52bd7893a735fae9e785efdee63547f0269c')
--- task2/task2.py
+++ task2/task2.py
@@ -1,15 +1,76 @@
 from hash_utils import *
+import random, string
 
 # Write your code to generate two letters here
+base_text = "NoWorkLifeBalance, Inc. Bob should be "
+# alice_text = "NoWorkLifeBalance, Inc. Bob should be promoted"
+# bob_text = "NoWorkLifeBalance, Inc. Bob should be fired"
+min_sub = 10000000
+hash_dict = {}
+# alice_hash = shf64(alice_text)
+# bob_hash = shf64(bob_text)
+base_hash = shf64(base_text)
+
+# store (hash key and value in table)
+# hash_dict[alice_hash] = alice_text
+# hash_dict[bob_hash] = bob_text
+hash_dict[base_hash] = base_text
 
 
+
+while(True):
+    
+    # if(alice_hash==bob_hash):
+    #     shf64_alice = alice_text
+    #     shf64_bob = bob_text
+    #     break
+    promoted = "promoted"
+    fired = "fired"
+    
+    N = random.randint(1,8)
+    M = random.randint(1,8)
+
+    random_str1 = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
+    random_str2 = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(M))
+    str1_hash = shf64(random_str1)
+    str2_hash = shf64(random_str2)
+
+    if(random_str1 == random_str2):
+        continue
+
+    if(str1_hash == str2_hash):
+        print("collision happens!")
+        print("random_str1",random_str1)
+        print("random_str2",random_str2)
+
+        str = hash_dict[str1_hash]
+        break
+
+    elif(str1_hash in hash_dict.keys() and random_str1 != hash_dict[str1_hash]):
+        print("collision happens!")
+        str = hash_dict[str1_hash]
+        print("random_str1",random_str1 )
+        print("random_str2",str )
+        break
+
+    elif(str2_hash in hash_dict.keys() and random_str2 != hash_dict[str2_hash]):
+        print("collision happens!")
+        str = hash_dict[str2_hash]    
+        print("random_str2",random_str2)
+        print("random_str2",str)
+        break
+
+    hash_dict[str1_hash]=random_str1
+    hash_dict[str2_hash]=random_str2    
+
+    
 
 # Write your solution here
-shf24_alice = None
-shf24_bob = None
+shf64_alice = None
+shf64_bob = None
 
 shf64_alice = None
 shf64_bob = None
 
-shf96_alice = None
-shf96_bob = None
+shf64_alice = None
+shf64_bob = None
