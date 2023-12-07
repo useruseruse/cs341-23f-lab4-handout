4e99ca7a655b35665c8d17e2858dd0ed3d650364
useruseruse <eliza1102@kaist.ac.kr>
2023-12-08 00:38:34 +0900

    task2

--- task2/task2.py
+++ task2/task2.py
@@ -6,7 +6,9 @@ base_text = "NoWorkLifeBalance, Inc. Bob should be "
 # alice_text = "NoWorkLifeBalance, Inc. Bob should be promoted"
 # bob_text = "NoWorkLifeBalance, Inc. Bob should be fired"
 min_sub = 10000000
-hash_dict = {}
+hash_dict_promoted = {}
+hash_dict_fired = {}
+
 # alice_hash = shf64(alice_text)
 # bob_hash = shf64(bob_text)
 base_hash = shf64(base_text)
@@ -14,7 +16,7 @@ base_hash = shf64(base_text)
 # store (hash key and value in table)
 # hash_dict[alice_hash] = alice_text
 # hash_dict[bob_hash] = bob_text
-hash_dict[base_hash] = base_text
+
 
 
 
@@ -24,14 +26,14 @@ while(True):
     #     shf64_alice = alice_text
     #     shf64_bob = bob_text
     #     break
-    promoted = "promoted"
-    fired = "fired"
+    promoted = "promoted "
+    fired = "fired "
     
     N = random.randint(1,8)
     M = random.randint(1,8)
 
-    random_str1 = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
-    random_str2 = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(M))
+    random_str1 = promoted +''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
+    random_str2 = fired + ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(M))
     str1_hash = shf64(random_str1)
     str2_hash = shf64(random_str2)
 
@@ -39,38 +41,42 @@ while(True):
         continue
 
     if(str1_hash == str2_hash):
-        print("collision happens!")
+        print("case 1: collision happens!")
         print("random_str1",random_str1)
         print("random_str2",random_str2)
 
-        str = hash_dict[str1_hash]
+        str = hash_dict_promoted[str1_hash]
+        str2 = hash_dict_fired[str2_hash]
+
         break
 
-    elif(str1_hash in hash_dict.keys() and random_str1 != hash_dict[str1_hash]):
+    elif(str1_hash in hash_dict_fired.keys()):
         print("collision happens!")
-        str = hash_dict[str1_hash]
+        str = hash_dict_fired[str1_hash]
         print("random_str1",random_str1 )
         print("random_str2",str )
         break
 
-    elif(str2_hash in hash_dict.keys() and random_str2 != hash_dict[str2_hash]):
+    # fired 의 hash 가 promoted 의 hash dict에 있는 어떤 값과 일치할 경우. 
+    elif(str2_hash in hash_dict_promoted.keys()):
         print("collision happens!")
-        str = hash_dict[str2_hash]    
+        str = hash_dict_promoted[str2_hash]   
+       
         print("random_str2",random_str2)
         print("random_str2",str)
         break
 
-    hash_dict[str1_hash]=random_str1
-    hash_dict[str2_hash]=random_str2    
-
+ 
+    hash_dict_promoted[str1_hash] = random_str1
+    hash_dict_fired[str2_hash] = random_str2
     
 
 # Write your solution here
-shf64_alice = None
-shf64_bob = None
+shf24_alice = None
+shf24_bob = None
 
 shf64_alice = None
 shf64_bob = None
 
-shf64_alice = None
-shf64_bob = None
+shf96_alice = None
+shf96_bob = None
